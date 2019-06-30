f8faaa9864c96847bb56606912921673fd7a15be
Max Bruckner <max@maxbruckner.de>
2019-06-30 21:50:58 +0200

    Simplify decoding of response

--- communityvi-lib/tests/server_test.rs
+++ communityvi-lib/tests/server_test.rs
@@ -1,6 +1,5 @@
 use communityvi_lib::server::create_server;
 use futures::future::Future;
-use futures::stream::Stream;
 use std::fmt::Debug;
 use tokio::runtime::Runtime;
 
@@ -9,16 +8,9 @@ fn should_get_offset() {
 	let client = reqwest::r#async::Client::new();
 	let get_request = client.get("http://localhost:8000").build().unwrap();
 
-	let future = client
-		.execute(get_request)
-		.and_then(|response| response.into_body().concat2().map(|bytes| bytes.to_vec()))
-		.map(|bytes| String::from_utf8(bytes).unwrap())
-		.map(|string| {
-			let number: u64 = string.parse().unwrap();
-			number
-		});
+	let future = client.execute(get_request).and_then(|mut response| response.json());
 
-	let offset = test_future_with_running_server(future);
+	let offset: u64 = test_future_with_running_server(future);
 	assert_eq!(offset, 42);
 }
 
