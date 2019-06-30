ca5efe871ea0f38f9fd52b20b5ec08989d0ffda7
Max Bruckner <max@maxbruckner.de>
2019-06-30 23:55:50 +0200

    Implement websocket echo server

--- communityvi-lib/src/server.rs
+++ communityvi-lib/src/server.rs
@@ -1,10 +1,12 @@
 use crate::state::State;
+use futures::stream::Stream;
 use futures::Future;
 use std::convert::Into;
 use std::net::SocketAddr;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use warp::filters::ws::Ws2;
 use warp::Filter;
 
 pub fn create_server<ShutdownHandleType>(
@@ -27,7 +29,21 @@ where
 		.map(move |new_offset| state_for_post.offset.store(new_offset, Ordering::SeqCst))
 		.map(|_| http::response::Builder::new().status(204).body(""));
 
-	let filter = get_state.or(post_state);
+	let websocket_filter = warp::path("ws").and(warp::ws2()).map(|ws2: Ws2| {
+		ws2.on_upgrade(|websocket| {
+			let (sink, stream) = websocket.split();
+			stream
+				.inspect(|message| println!("{:?}", message))
+				.take_while(|message| futures::future::ok(!message.is_close()))
+				.forward(sink)
+				.map(|_| ())
+				.map_err(|error| {
+					eprintln!("{}", error);
+				})
+		})
+	});
+
+	let filter = websocket_filter.or(get_state).or(post_state);
 	let server = warp::serve(filter);
 
 	let (_address, future) = server.bind_with_graceful_shutdown(address, shutdown_handle);
