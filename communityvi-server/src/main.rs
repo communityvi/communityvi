bae5a16062bf910043d0ddf0a0b5e4db0597c83b
Max Bruckner <max@maxbruckner.de>
2019-11-27 01:51:34 +0100

    First step towards async await: futures 0.3 channel and tokio compat runtime

--- communityvi-server/src/main.rs
+++ communityvi-server/src/main.rs
@@ -1,7 +1,9 @@
 use communityvi_lib::server::create_server;
+use futures::{FutureExt, TryFutureExt};
 
 fn main() {
-	let (_sender, receiver) = futures::sync::oneshot::channel();
-	let server = create_server(([127, 0, 0, 1], 8000), receiver);
-	tokio::run(server);
+	let (_sender, receiver) = futures::channel::oneshot::channel::<()>();
+	let server = create_server(([127, 0, 0, 1], 8000), receiver.boxed().compat());
+
+	tokio_compat::run(server);
 }
