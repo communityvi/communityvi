f36bea59db087399a4bf0aea9e309135454c170a
Max Bruckner <max@maxbruckner.de>
2020-03-25 21:27:58 +0100

    Main: remove tokio::spawn

--- communityvi-server/src/main.rs
+++ communityvi-server/src/main.rs
@@ -10,11 +10,11 @@ mod server_tests;
 mod state;
 
 #[tokio::main]
-async fn main() -> Result<(), JoinError> {
+async fn main() {
 	env_logger::init();
 	let (_shutdown_sender, shutdown_receiver) = futures::channel::oneshot::channel::<()>();
 	let shutdown_handle = shutdown_receiver.then(|_| futures::future::ready(()));
 	let server = create_server(([127, 0, 0, 1], 8000), shutdown_handle);
 
-	tokio::spawn(server).await
+	server.await;
 }
