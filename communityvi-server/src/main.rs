98c584badd196fd0f8a180c0c176dbe50e7b95c0
Max Bruckner <max@maxbruckner.de>
2020-03-27 23:43:06 +0100

    Load configuration on server startup

--- communityvi-server/src/main.rs
+++ communityvi-server/src/main.rs
@@ -1,3 +1,5 @@
+use crate::configuration::Configuration;
+use crate::error::CommunityviError;
 use crate::server::create_server;
 use futures::FutureExt;
 
@@ -5,6 +7,7 @@ mod atomic_sequence;
 mod client;
 mod client_id_sequence;
 mod configuration;
+mod error;
 mod message;
 mod room;
 mod server;
@@ -12,11 +15,16 @@ mod server;
 mod server_tests;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), CommunityviError> {
+	const CONFIGURATION_FILE_PATH: &str = "configuration.toml";
+	let configuration = Configuration::from_file(CONFIGURATION_FILE_PATH)?;
+
 	env_logger::init();
+
 	let (_shutdown_sender, shutdown_receiver) = futures::channel::oneshot::channel::<()>();
 	let shutdown_handle = shutdown_receiver.then(|_| futures::future::ready(()));
-	let server = create_server(([127, 0, 0, 1], 8000), shutdown_handle);
+	let server = create_server(configuration.address, shutdown_handle);
 
 	server.await;
+	Ok(())
 }
