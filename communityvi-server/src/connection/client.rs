a417cbe7a2327eb4540e346f407fde1e2e0f9996
Max Bruckner <max@maxbruckner.de>
2020-04-10 17:06:21 +0200

    Make ClientConnection generic

--- communityvi-server/src/connection/client.rs
+++ communityvi-server/src/connection/client.rs
@@ -1,25 +1,38 @@
 use crate::message::{OrderedMessage, ServerResponse, WebSocketMessage};
 use futures::stream::SplitSink;
+use futures::Sink;
 use futures::SinkExt;
+use std::fmt::Debug;
 use std::ops::Range;
 use std::sync::Arc;
 use warp::ws::WebSocket;
 
-#[derive(Clone, Debug)]
-pub struct ClientConnection {
-	inner: Arc<tokio::sync::Mutex<ClientConnectionInner>>,
+#[derive(Debug)]
+pub struct ClientConnection<ResponseSink = SplitSink<WebSocket, WebSocketMessage>> {
+	inner: Arc<tokio::sync::Mutex<ClientConnectionInner<ResponseSink>>>,
+}
+
+impl<T> Clone for ClientConnection<T> {
+	fn clone(&self) -> Self {
+		ClientConnection {
+			inner: self.inner.clone(),
+		}
+	}
 }
 
 #[derive(Debug)]
-struct ClientConnectionInner {
-	websocket_sink: SplitSink<WebSocket, WebSocketMessage>,
+struct ClientConnectionInner<ResponseSink> {
+	response_sink: ResponseSink,
 	message_number_sequence: Range<u64>,
 }
 
-impl ClientConnection {
-	pub fn new(websocket_sink: SplitSink<WebSocket, WebSocketMessage>) -> Self {
+impl<ResponseSink> ClientConnection<ResponseSink>
+where
+	ResponseSink: Sink<WebSocketMessage> + Unpin,
+{
+	pub fn new(response_sink: ResponseSink) -> Self {
 		let inner = ClientConnectionInner {
-			websocket_sink,
+			response_sink,
 			message_number_sequence: (0..std::u64::MAX),
 		};
 		Self {
@@ -36,11 +49,11 @@ impl ClientConnection {
 		};
 		let websocket_message = WebSocketMessage::from(&ordered_message);
 
-		inner.websocket_sink.send(websocket_message).await.map_err(|_| ())
+		inner.response_sink.send(websocket_message).await.map_err(|_| ())
 	}
 
 	pub async fn close(&self) {
 		let mut inner = self.inner.lock().await;
-		let _ = inner.websocket_sink.send(WebSocketMessage::close()).await;
+		let _ = inner.response_sink.send(WebSocketMessage::close()).await;
 	}
 }
