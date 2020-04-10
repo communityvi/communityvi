8472be38f08190c880ea842921dd8a5b15c85284
Max Bruckner <max@maxbruckner.de>
2020-04-10 19:40:29 +0200

    Abstract ServerConnection via Trait

--- communityvi-server/src/connection.rs
+++ communityvi-server/src/connection.rs
@@ -1,5 +1,5 @@
 use crate::connection::client::{ClientConnection, WebSocketClientConnection};
-use crate::connection::server::ServerConnection;
+use crate::connection::server::{ServerConnection, WebSocketServerConnection};
 use crate::infallible_stream::InfallibleStream;
 use futures::StreamExt;
 use warp::filters::ws::WebSocket;
@@ -10,9 +10,9 @@ pub mod server;
 pub fn split_websocket(websocket: WebSocket) -> (ClientConnection, ServerConnection) {
 	let (websocket_sink, websocket_stream) = websocket.split();
 	let websocket_client_connection = WebSocketClientConnection::new(websocket_sink);
-	let server_connection = ServerConnection::new(
+	let stream_server_connection = WebSocketServerConnection::new(
 		InfallibleStream::from(websocket_stream),
 		websocket_client_connection.clone().into(),
 	);
-	(websocket_client_connection.into(), server_connection)
+	(websocket_client_connection.into(), stream_server_connection.into())
 }
--- communityvi-server/src/connection/server.rs
+++ communityvi-server/src/connection/server.rs
@@ -1,30 +1,34 @@
 use crate::connection::client::ClientConnection;
 use crate::infallible_stream::InfallibleStream;
 use crate::message::{ClientRequest, ErrorResponse, MessageError, OrderedMessage, ServerResponse, WebSocketMessage};
+use async_trait::async_trait;
 use futures::stream::SplitStream;
 use futures::{Stream, StreamExt};
 use log::error;
 use std::convert::TryFrom;
+use std::pin::Pin;
 use warp::ws::WebSocket;
 
-pub struct ServerConnection<RequestStream = InfallibleStream<SplitStream<WebSocket>>> {
+pub type ServerConnection = Pin<Box<dyn ServerConnectionTrait + Unpin + Send>>;
+pub type WebSocketServerConnection = StreamServerConnection<InfallibleStream<SplitStream<WebSocket>>>;
+
+#[async_trait]
+pub trait ServerConnectionTrait {
+	/// Receive a message from the client or None if the connection has been closed.
+	async fn receive(&mut self) -> Option<OrderedMessage<ClientRequest>>;
+}
+
+pub struct StreamServerConnection<RequestStream = InfallibleStream<SplitStream<WebSocket>>> {
 	request_stream: RequestStream,
 	client_connection: ClientConnection,
 }
 
-impl<RequestStream> ServerConnection<RequestStream>
+#[async_trait]
+impl<RequestStream> ServerConnectionTrait for StreamServerConnection<RequestStream>
 where
-	RequestStream: Stream<Item = WebSocketMessage> + Unpin,
+	RequestStream: Stream<Item = WebSocketMessage> + Unpin + Send,
 {
-	pub fn new(request_stream: RequestStream, client_connection: ClientConnection) -> Self {
-		Self {
-			request_stream,
-			client_connection,
-		}
-	}
-
-	/// Receive a message from the client or None if the connection has been closed.
-	pub async fn receive(&mut self) -> Option<OrderedMessage<ClientRequest>> {
+	async fn receive(&mut self) -> Option<OrderedMessage<ClientRequest>> {
 		const MAXIMUM_RETRIES: usize = 10;
 
 		for _ in 0..MAXIMUM_RETRIES {
@@ -73,5 +77,26 @@ where
 			.await;
 		let _ = self.client_connection.close().await;
 		None
+	}
+}
+
+impl<RequestStream> StreamServerConnection<RequestStream>
+where
+	RequestStream: Stream<Item = WebSocketMessage>,
+{
+	pub fn new(request_stream: RequestStream, client_connection: ClientConnection) -> Self {
+		Self {
+			request_stream,
+			client_connection,
+		}
+	}
+}
+
+impl<RequestStream> From<StreamServerConnection<RequestStream>> for ServerConnection
+where
+	RequestStream: Stream<Item = WebSocketMessage> + Unpin + Send + 'static,
+{
+	fn from(stream_server_connection: StreamServerConnection<RequestStream>) -> Self {
+		Box::pin(stream_server_connection)
 	}
 }
