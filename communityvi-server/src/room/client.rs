95c7a45e73ba9f423de93deafcc3dd3852597db2
Max Bruckner <max@maxbruckner.de>
2020-04-24 22:33:13 +0200

    Remove debug_stub_derive

--- communityvi-server/src/room/client.rs
+++ communityvi-server/src/room/client.rs
@@ -1,9 +1,6 @@
 use crate::connection::client::ClientConnection;
-use debug_stub_derive::DebugStub;
 
-#[derive(DebugStub)]
 pub struct Client {
 	pub name: String,
-	#[debug_stub = "ClientConnection"]
 	pub connection: ClientConnection,
 }
