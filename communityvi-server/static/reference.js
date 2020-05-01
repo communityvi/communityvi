446e1e8c0bcccf84d92041ad882f6a94a1a02bbb
Thomas Keppler <winfr34k@gmail.com>
2020-05-01 21:59:35 +0200

    Add Playback and Pause handling in Reference Client

--- communityvi-server/static/reference.js
+++ communityvi-server/static/reference.js
@@ -22,8 +22,51 @@ let referenceTimeOffset = null;
 let referenceTime = null;
 const referenceTimeDisplay = document.getElementById('reference_time');
 
-setupButtonPressOnEnter();
+let mediumLength = null;
+let playbackState = {
+	type: 'paused',
+	startTime: 0,
+	position: 0,
+};
+const mediumNameLabel = document.getElementById('medium_name');
+const mediumLengthLabel = document.getElementById('medium_length');
+const playerPositionLabel = document.getElementById('player_position');
+const playerPositionSlider = document.getElementById('player_position_slider');
+const insertMediumButton = document.getElementById('insert_medium');
+insertMediumButton.onclick = function () {
+	if (webSocket === null) {
+		return;
+	}
 
+	const name = prompt('What should the fake medium be called?', 'Birdman');
+	const length = Number.parseInt(prompt('How long in minutes should the fake medium be?', '116')) * 60 * 1000;
+	sendMessage({type: 'insert_medium', name: name, length_in_milliseconds: length});
+};
+const playPauseButton = document.getElementById('play_pause');
+playPauseButton.onclick = function () {
+	if (mediumLength === null) {
+		return;
+	}
+
+	if (playbackState.type === 'paused') {
+		playbackState.type = 'playing';
+		playbackState.startTime = calculateReferenceTime() - playbackState.position;
+
+		sendMessage({type: 'play', start_time_in_milliseconds: Math.round(playbackState.startTime), skipped: false});
+		playPauseButton.innerHTML = '&#9208;';
+	} else if (playbackState.type === 'playing') {
+		playbackState.type = 'paused';
+		playbackState.position = calculateReferenceTime() - playbackState.startTime;
+		console.log(`calc ${calculateReferenceTime()} start ${playbackState.startTime} POS ${playbackState.position}`);
+
+		sendMessage({type: 'pause', position_in_milliseconds: Math.round(playbackState.position), skipped: false});
+		playPauseButton.innerHTML = '&#9654;';
+	}
+};
+const rewind10SecondsButton = document.getElementById('rewind_10');
+const forward10SecondsButton = document.getElementById('forward_10');
+
+setupButtonPressOnEnter();
 
 function setupButtonPressOnEnter() {
 	const nameField = document.getElementById('name');
@@ -98,7 +141,7 @@ function handleMessage(message, messageEvent) {
 			idField.innerText = message.id;
 
 			// start counter management
-			setInterval(displayCounter, 16);
+			setInterval(updateApplicationState, 16);
 			requestReferenceTime();
 			setInterval(requestReferenceTime, 10000);
 			break;
@@ -145,6 +188,48 @@ function handleMessage(message, messageEvent) {
 			break;
 		}
 
+		case 'medium_inserted': {
+		    mediumNameLabel.textContent = message.name;
+		    mediumLengthLabel.textContent = message.length_in_milliseconds / 1000 / 60;
+
+		    playbackState.type = 'paused';
+		    playbackState.position = 0;
+
+		    playerPositionSlider.max = message.length_in_milliseconds;
+		    playerPositionSlider.value = 0;
+		    playerPositionLabel.textContent = 0;
+
+		    displayChatMessage(message.inserted_by_id, message.inserted_by_name, `<< inserted "${message.name}" >>`);
+
+			mediumLength = message.length_in_milliseconds;
+
+			break;
+		}
+
+		case 'playback_state_changed': {
+		    playbackState.type = message.playback_state.type;
+			if (message.playback_state.type === 'playing') {
+				playbackState.startTime = message.playback_state.start_time_in_milliseconds;
+				playPauseButton.innerHTML = '&#9208;';
+				displayChatMessage('', 'Server', `${message.changed_by_name} (Client ID: ${message.changed_by_id}) started playback.`);
+			} else {
+				playbackState.position = message.playback_state.position_in_milliseconds;
+				playPauseButton.innerHTML = '&#9654;';
+				displayChatMessage('', 'Server', `${message.changed_by_name} (Client ID: ${message.changed_by_id}) paused playback.`);
+			}
+
+			break;
+		}
+
+		case 'error': {
+			console.error(`Received error message: [${message.error}] '${message.message}'`);
+			break;
+		}
+
+		default: {
+			console.error(`UNKNOWN MESSAGE TYPE: '${message.type}'!`);
+			break;
+		}
 	}
 }
 
@@ -175,13 +260,32 @@ function displayChatMessage(id, name, message) {
 	row.insertCell().appendChild(document.createTextNode(message));
 }
 
-function displayCounter() {
+function updateApplicationState() {
 	if (referenceTime == null) {
 		return;
 	}
 
-	referenceTime = performance.now() + referenceTimeOffset;
+	referenceTime = calculateReferenceTime();
 	referenceTimeDisplay.innerText = `${Math.round(referenceTime)} ms`;
+
+	if (playbackState.type === 'playing') {
+	    const currentPosition = referenceTime - playbackState.startTime;
+
+	    if (currentPosition >= mediumLength) {
+	    	sendMessage({type: 'pause', position_in_milliseconds: mediumLength, skipped: false});
+	    	playbackState.position = mediumLength;
+			playerPositionSlider.value = playbackState.position;
+			playerPositionLabel.textContent = playbackState.position / 1000;
+	    	playbackState.type = 'paused';
+		} else {
+			playerPositionSlider.value = Math.round(currentPosition);
+			playerPositionLabel.textContent = currentPosition / 1000;
+		}
+	}
+}
+
+function calculateReferenceTime() {
+	return performance.now() + referenceTimeOffset;
 }
 
 function requestReferenceTime() {
