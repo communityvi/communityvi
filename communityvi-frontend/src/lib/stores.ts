f295bb2c458a1e5f44254f7a9f99cf738c037aa8
Thomas Keppler <winfr34k@gmail.com>
2021-06-12 23:44:37 +0200

    Create an ErrorBag store to hold any error.
    This makes errors identifiable so that they can be properly accessed in {#each} blocks.

--- communityvi-frontend/src/lib/stores.ts
+++ communityvi-frontend/src/lib/stores.ts
@@ -1,4 +1,6 @@
 import {Writable, writable} from 'svelte/store';
 import type {RegisteredClient} from '$lib/client/client';
+import {ErrorBag} from '$lib/error_bag';
 
 export const registeredClient: Writable<RegisteredClient | undefined> = writable(undefined);
+export const errorBag = new ErrorBag();
