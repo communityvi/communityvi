1bebb616da59f6b11386cba4a839a41aecf88f97
Max Bruckner <max@maxbruckner.de>
2021-07-30 22:09:32 +0200

    Pull out duplicate code in ReferenceTimeSynchronizer tests

--- communityvi-frontend/tests/client/reference_time_synchronizer.spec.ts
+++ communityvi-frontend/tests/client/reference_time_synchronizer.spec.ts
@@ -1,8 +1,8 @@
 import ReferenceTimeSynchronizer, {AlreadyRunningError} from '$lib/client/reference_time_synchronizer';
 import type {Connection} from '$lib/client/connection';
 import {EnrichedResponse, ResponseMetadata} from '$lib/client/connection';
-import {isA, mock, mockReset} from 'jest-mock-extended';
-import {GetReferenceTimeRequest} from '$lib/client/request';
+import {CalledWithMock, isA, mock, mockReset} from 'jest-mock-extended';
+import {ClientRequest, GetReferenceTimeRequest} from '$lib/client/request';
 import type {ReferenceTimeMessage} from '$lib/client/response';
 import {SuccessMessageType} from '$lib/client/response';
 import TimeMock from './helper/time_mock';
@@ -10,15 +10,7 @@ import TimeMock from './helper/time_mock';
 describe('The reference time synchronizer', () => {
 	it('should not allow starting synchronization twice', async () => {
 		const connectionMock = mock<Connection>();
-		connectionMock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
-			new EnrichedResponse(
-				<ReferenceTimeMessage>{
-					type: SuccessMessageType.ReferenceTime,
-					milliseconds: 1337,
-				},
-				new ResponseMetadata(0, 0),
-			),
-		);
+		scheduleReferenceTimeResponse(connectionMock, 1337, 0, 0);
 
 		const referenceTimeSynchronizer = await ReferenceTimeSynchronizer.createInitializedWithConnection(connectionMock);
 
@@ -29,15 +21,7 @@ describe('The reference time synchronizer', () => {
 
 	it('should have the correct initial offset after construction', async () => {
 		const connectionMock = mock<Connection>();
-		connectionMock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
-			new EnrichedResponse(
-				<ReferenceTimeMessage>{
-					type: SuccessMessageType.ReferenceTime,
-					milliseconds: 1337 + (1000 - 0) / 2,
-				},
-				new ResponseMetadata(0, 1000),
-			),
-		);
+		scheduleReferenceTimeResponse(connectionMock, 1337, 0, 1000);
 
 		const referenceTimeSynchronizer = await ReferenceTimeSynchronizer.createInitializedWithConnection(connectionMock);
 
@@ -47,31 +31,14 @@ describe('The reference time synchronizer', () => {
 	it('should inform its subscriber about offset updates', async () => {
 		await TimeMock.run(async (timeMock: TimeMock) => {
 			const connectionMock = mock<Connection>();
-			connectionMock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
-				new EnrichedResponse(
-					<ReferenceTimeMessage>{
-						type: SuccessMessageType.ReferenceTime,
-						milliseconds: 1337 + (1000 - 0) / 2,
-					},
-					new ResponseMetadata(0, 1000),
-				),
-			);
+			scheduleReferenceTimeResponse(connectionMock, 1337, 0, 1000);
 			const referenceTimeSynchronizer = await ReferenceTimeSynchronizer.createInitializedWithConnection(connectionMock);
 
 			const subscriber = jest.fn();
 			referenceTimeSynchronizer.start(subscriber);
 
 			// 15s passed and server and client are out of sync by 230ms.
-			mockReset(connectionMock);
-			connectionMock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
-				new EnrichedResponse(
-					<ReferenceTimeMessage>{
-						type: SuccessMessageType.ReferenceTime,
-						milliseconds: 16_337 + (16_000 - 15_000) / 2 + 230,
-					},
-					new ResponseMetadata(15_000, 16_000),
-				),
-			);
+			scheduleReferenceTimeResponse(connectionMock, 16_337 + 230, 15_000, 16_000);
 			await timeMock.advanceTimeByMilliseconds(15_000);
 
 			expect(setInterval).toHaveBeenCalledTimes(1);
@@ -82,31 +49,14 @@ describe('The reference time synchronizer', () => {
 	it('should not inform its subscriber if the offset stays the same', async () => {
 		await TimeMock.run(async (timeMock: TimeMock) => {
 			const connectionMock = mock<Connection>();
-			connectionMock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
-				new EnrichedResponse(
-					<ReferenceTimeMessage>{
-						type: SuccessMessageType.ReferenceTime,
-						milliseconds: 1337 + (1000 - 0) / 2,
-					},
-					new ResponseMetadata(0, 1000),
-				),
-			);
+			scheduleReferenceTimeResponse(connectionMock, 1337, 0, 1000);
 			const referenceTimeSynchronizer = await ReferenceTimeSynchronizer.createInitializedWithConnection(connectionMock);
 
 			const subscriber = jest.fn();
 			referenceTimeSynchronizer.start(subscriber);
 
 			// 15s passed and server and client are in sync.
-			mockReset(connectionMock);
-			connectionMock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
-				new EnrichedResponse(
-					<ReferenceTimeMessage>{
-						type: SuccessMessageType.ReferenceTime,
-						milliseconds: 16_337 + (16_000 - 15_000) / 2,
-					},
-					new ResponseMetadata(15_000, 16_000),
-				),
-			);
+			scheduleReferenceTimeResponse(connectionMock, 16_337, 15_000, 16_000);
 			await timeMock.advanceTimeByMilliseconds(15_000);
 
 			expect(setInterval).toHaveBeenCalledTimes(1);
@@ -114,3 +64,23 @@ describe('The reference time synchronizer', () => {
 		});
 	});
 });
+
+function scheduleReferenceTimeResponse(
+	mock: ConnectionMock,
+	referenceTime: number,
+	sentAt: number,
+	receivedAt: number,
+) {
+	mockReset(mock);
+	mock.performRequest.calledWith(isA(GetReferenceTimeRequest)).mockResolvedValueOnce(
+		new EnrichedResponse(
+			<ReferenceTimeMessage>{
+				type: SuccessMessageType.ReferenceTime,
+				milliseconds: referenceTime + (receivedAt - sentAt) / 2,
+			},
+			new ResponseMetadata(sentAt, receivedAt),
+		),
+	);
+}
+
+type ConnectionMock = {performRequest: CalledWithMock<Promise<EnrichedResponse>, [ClientRequest]>} & Connection;
