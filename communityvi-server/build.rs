aa5d2863dfdcf8a7be3e23689914dc539d0df1f6
Max Bruckner <max@maxbruckner.de>
2021-10-11 01:51:25 +0200

    Only rerun frontend build if necessary

--- communityvi-server/build.rs
+++ communityvi-server/build.rs
@@ -1,11 +1,17 @@
 use npm_rs::NpmEnv;
 use std::env;
+use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let frontend_path = Path::new("../communityvi-frontend");
 	match env::var("CARGO_FEATURE_BUNDLE_FRONTEND") {
 		Ok(_) => {
+			if is_debug_profile() {
+				rerun_if_frontend_changes(&frontend_path);
+			}
+
 			let exit_status = NpmEnv::default()
-				.set_path("../communityvi-frontend")
+				.set_path(frontend_path)
 				.init_env()
 				.install(None)
 				.run("build")
@@ -15,8 +21,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			}
 		}
 		Err(_) => {
+			// don't always rerun build.rs
 			println!("cargo:rerun-if-changed=build.rs")
 		}
 	}
 	Ok(())
+}
+
+fn is_debug_profile() -> bool {
+	env::var("PROFILE") == Ok("debug".to_string())
+}
+
+// Prints the necessary cargo directives for rebuilding only
+// if changes to the frontend directory have been detected.
+fn rerun_if_frontend_changes(frontend_path: &'static Path) {
+	let package_lock_json_path = frontend_path.join("package-lock.json");
+
+	for result in ignore::WalkBuilder::new(frontend_path)
+		.hidden(false)
+		.ignore(false)
+		.require_git(false)
+		.build()
+	{
+		let entry = result.unwrap();
+		let path = entry.path();
+
+		// These paths are touched by npm, so ignore them
+		if (path == package_lock_json_path) || (path == frontend_path) {
+			continue;
+		}
+
+		println!("cargo:rerun-if-changed={}", path.display());
+	}
 }
