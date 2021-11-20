dadaf12888ef281c0b92301ae88af217086b00fd
Max Bruckner <max@maxbruckner.de>
2021-11-20 12:15:07 +0100

    Switch to the latest version of js_int that supports deserializing from floats

---
+++ communityvi-server/tests/learning_tests.rs
@@ -0,0 +1,22 @@
+use js_int::{int, uint, Int, UInt};
+
+#[test]
+fn js_int_supports_deserializing_from_floats() {
+	let positive_json = "42.0";
+	let negative_json = "-42.0";
+
+	let uint = serde_json::from_str::<UInt>(positive_json).unwrap();
+	let int = serde_json::from_str::<Int>(negative_json).unwrap();
+
+	assert_eq!(uint, uint!(42));
+	assert_eq!(int, int!(-42));
+}
+
+#[test]
+fn js_int_declines_deserializing_from_fractional_float() {
+	let positive_json = "42.5";
+	let negative_json = "-42.5";
+
+	serde_json::from_str::<UInt>(positive_json).expect_err("Deserialized UInt although it shouldn't");
+	serde_json::from_str::<Int>(negative_json).expect_err("Deserialized Int although it shouldn't");
+}
