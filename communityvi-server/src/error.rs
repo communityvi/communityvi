0db16f1e2110e222942d85ae0a9eabbd392b4709
Max Bruckner <max@maxbruckner.de>
2021-12-30 15:04:39 +0100

    Use thiserror for our errors

--- communityvi-server/src/error.rs
+++ communityvi-server/src/error.rs
@@ -1,34 +1,10 @@
 use crate::configuration::ConfigurationError;
-use std::error::Error;
-use std::fmt::{Display, Formatter};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum CommunityviError {
-	ConfigurationError(ConfigurationError),
-	CommandlineError(String),
-}
-
-impl Display for CommunityviError {
-	fn fmt(&self, formatter: &mut Formatter) -> std::fmt::Result {
-		match self {
-			CommunityviError::ConfigurationError(error) => write!(formatter, "Failed to load configuration: {}", error),
-			CommunityviError::CommandlineError(message) => {
-				write!(formatter, "Failed to parse commandline: {}", message)
-			}
-		}
-	}
-}
-
-impl Error for CommunityviError {}
-
-impl From<ConfigurationError> for CommunityviError {
-	fn from(configuration_error: ConfigurationError) -> Self {
-		CommunityviError::ConfigurationError(configuration_error)
-	}
-}
-
-impl From<clap::Error> for CommunityviError {
-	fn from(clap_error: clap::Error) -> Self {
-		CommunityviError::CommandlineError(clap_error.to_string())
-	}
+	#[error("Failed to load configuration: {0}")]
+	ConfigurationError(#[from] ConfigurationError),
+	#[error("Failed to parse commandline: {0}")]
+	CommandlineError(#[from] clap::Error),
 }
--- communityvi-server/src/message.rs
+++ communityvi-server/src/message.rs
@@ -1,32 +1,15 @@
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::Debug;
+use thiserror::Error;
 
 pub mod client_request;
 pub mod outgoing;
 
 pub type WebSocketMessage = tokio_tungstenite::tungstenite::Message;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum MessageError {
+	#[error("Failed to deserialize message with error: '{}'; Message was '{}'", .error, .json)]
 	DeserializationFailed { error: String, json: String },
+	#[error("Wrong websocket message type. Epected text, got: {0:?}")]
 	WrongMessageType(WebSocketMessage),
 }
-
-impl Display for MessageError {
-	fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
-		match self {
-			MessageError::DeserializationFailed { error, json } => write!(
-				formatter,
-				"Failed to deserialize message with error: '{}'; Message was '{}'",
-				error, json
-			),
-			MessageError::WrongMessageType(message) => write!(
-				formatter,
-				"Wrong websocket message type. Expected text, got: {:?}",
-				message
-			),
-		}
-	}
-}
-
-impl Error for MessageError {}
