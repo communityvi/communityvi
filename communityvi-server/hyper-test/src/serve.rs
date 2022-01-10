95fe7825ea4d46ccc22e6abf9590d2d66cedfdca
Max Bruckner <max@maxbruckner.de>
2022-01-11 00:51:04 +0100

    hyper-test package for in-memory tests of hyper services

---
+++ communityvi-server/hyper-test/src/serve.rs
@@ -0,0 +1,38 @@
+use crate::{connection, ConnectionIncoming, Connector, Host};
+use hyper::body::HttpBody;
+use hyper::service::Service;
+use hyper::{Body, Request, Response, Server};
+use std::future::Future;
+use tokio::sync::mpsc;
+
+pub fn serve<MakeService, HttpService, ResponseBody, MakeError, MakeFuture>(
+	make_service: MakeService,
+	bind_host: Option<Host>,
+) -> (
+	hyper::Client<Connector, Body>,
+	hyper::Server<ConnectionIncoming, MakeService>,
+)
+where
+	MakeService:
+		for<'a> Service<&'a connection::Connection, Response = HttpService, Error = MakeError, Future = MakeFuture>,
+	MakeError: std::error::Error + Send + Sync + 'static,
+	MakeFuture: Future<Output = Result<HttpService, MakeError>> + Send + 'static,
+	HttpService: Service<Request<Body>, Response = Response<ResponseBody>> + Send + 'static,
+	HttpService::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+	HttpService::Future: Send,
+	ResponseBody: HttpBody + Send + 'static,
+	ResponseBody::Data: Send,
+	ResponseBody::Error: std::error::Error + Send + Sync,
+{
+	let (sender, receiver) = mpsc::channel(1);
+	let incoming = ConnectionIncoming::new(receiver);
+	let server = Server::builder(incoming).serve(make_service);
+
+	let connector = match bind_host {
+		Some(host) => Connector::new(sender).bind_to_host(host),
+		None => Connector::new(sender),
+	};
+	let client = hyper::client::Client::builder().build(connector);
+
+	(client, server)
+}
