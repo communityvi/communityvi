d44f4918fdb8db8da9cb9a1c0b6c6530b1efcb21
Thomas Keppler <winfr34k@gmail.com>
2022-12-29 17:32:18 +0100

    Move Client's connection related data into its own struct.

--- communityvi-server/src/room/client.rs
+++ communityvi-server/src/room/client.rs
@@ -1,5 +1,6 @@
 use crate::connection::broadcast_buffer::BroadcastBuffer;
 use crate::connection::sender::MessageSender;
+use crate::connection::Connection;
 use crate::message::outgoing::broadcast_message::BroadcastMessage;
 use crate::message::outgoing::error_message::ErrorMessage;
 use crate::message::outgoing::success_message::SuccessMessage;
@@ -14,21 +15,16 @@ pub struct Client {
 }
 
 struct Inner {
-	pub id: ClientId,
-	pub name: String,
-	pub connection: MessageSender,
-	pub broadcast_buffer: BroadcastBuffer,
+	id: ClientId,
+	name: String,
+	connection: Connection,
 }
 
 impl Client {
-	pub fn new(id: ClientId, name: String, broadcast_buffer: BroadcastBuffer, connection: MessageSender) -> Self {
+	pub fn new(id: ClientId, name: String, broadcast_buffer: BroadcastBuffer, sender: MessageSender) -> Self {
+		let connection = Connection::new(sender, broadcast_buffer);
 		Self {
-			inner: Arc::new(Inner {
-				id,
-				name,
-				connection,
-				broadcast_buffer,
-			}),
+			inner: Arc::new(Inner { id, name, connection }),
 		}
 	}
 
@@ -41,76 +37,54 @@ impl Client {
 	}
 
 	pub async fn send_success_message(&self, message: SuccessMessage, request_id: UInt) -> bool {
-		if self
-			.inner
-			.connection
-			.send_success_message(message, request_id)
-			.await
-			.is_err()
-		{
+		let success = self.inner.connection.send_success_message(message, request_id).await;
+		if !success {
 			info!(
 				"Failed to send success message to client with id {} because it went away.",
 				self.inner.id
 			);
-			false
-		} else {
-			true
 		}
+		success
 	}
 
 	pub async fn send_error_message(&self, message: ErrorMessage, request_id: Option<UInt>) -> bool {
-		if self
-			.inner
-			.connection
-			.send_error_message(message, request_id)
-			.await
-			.is_err()
-		{
+		let success = self.inner.connection.send_error_message(message, request_id).await;
+		if !success {
 			info!(
 				"Failed to send error message to client with id {} because it went away.",
 				self.inner.id
 			);
-			false
-		} else {
-			true
 		}
+		success
 	}
 
 	pub async fn send_broadcast_message(&self, message: impl Into<BroadcastMessage> + Unpin) -> bool {
-		if self
-			.inner
-			.connection
-			.send_broadcast_message(message.into())
-			.await
-			.is_err()
-		{
+		let success = self.inner.connection.send_broadcast_message(message.into()).await;
+		if !success {
 			info!(
 				"Failed to send broadcast to client with id {} because it went away.",
 				self.inner.id
 			);
-			false
-		} else {
-			true
 		}
+		success
 	}
 
 	pub fn enqueue_broadcast(&self, message: impl Into<BroadcastMessage> + Unpin, count: usize) {
-		self.inner.broadcast_buffer.enqueue(message.into(), count);
+		self.inner.connection.enqueue_broadcast(message.into(), count);
 	}
 
 	pub async fn wait_for_broadcast(&self) -> BroadcastMessage {
-		self.inner.broadcast_buffer.wait_for_broadcast().await
+		self.inner.connection.wait_for_broadcast().await
 	}
 
 	pub async fn send_ping(&self, payload: Vec<u8>) -> bool {
-		if self.inner.connection.send_ping(payload).await.is_err() {
+		let success = self.inner.connection.send_ping(payload).await;
+		if !success {
 			info!(
 				"Failed to send ping to client with id {} because it went away.",
 				self.inner.id
 			);
-			false
-		} else {
-			true
 		}
+		success
 	}
 }
