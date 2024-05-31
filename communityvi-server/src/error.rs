0fb2038256504bb9b875629b09a559725b6ea942
Max Bruckner <max@maxbruckner.de>
2024-05-31 23:16:19 +0200

    Update axum and hyper

--- communityvi-server/src/error.rs
+++ communityvi-server/src/error.rs
@@ -4,7 +4,9 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum CommunityviError {
 	#[error("Failed to load configuration: {0}")]
-	ConfigurationError(#[from] ConfigurationError),
+	Configuration(#[from] ConfigurationError),
 	#[error("Failed to parse commandline: {0}")]
-	CommandlineError(#[from] clap::Error),
+	Commandline(#[from] clap::Error),
+	#[error("IO error while serving requests: {0}")]
+	Server(#[from] std::io::Error),
 }
