821652b5d9d884f0d2785eb9c428e5519aadbfaa
Max Bruckner <max@maxbruckner.de>
2025-12-21 18:38:27 +0100

    Implement ChatRepository for libsql

--- communityvi-server/src/chat/model.rs
+++ communityvi-server/src/chat/model.rs
@@ -1,5 +1,5 @@
+use crate::types::date_time::DateTime;
 use crate::types::uuid::Uuid;
-use chrono::{DateTime, Utc};
 use sqlx::FromRow;
 
 #[derive(FromRow, Clone, Debug, PartialEq, Eq)]
@@ -9,5 +9,31 @@ pub struct ChatMessage {
 	pub user_uuid: Option<Uuid>,
 	pub user_name: String,
 	pub message: String,
-	pub created_at: DateTime<Utc>,
+	pub created_at: DateTime,
+}
+
+impl TryFrom<libsql::Row> for ChatMessage {
+	type Error = anyhow::Error;
+
+	fn try_from(row: libsql::Row) -> Result<Self, Self::Error> {
+		let uuid = row.get_value(0)?;
+		let room_uuid = row.get_value(1)?;
+		let user_uuid = row.get_value(2)?;
+		let user_name = row.get(3)?;
+		let message = row.get(4)?;
+		let created_at = row.get_value(5)?;
+
+		Ok(Self {
+			uuid: uuid.try_into()?,
+			room_uuid: room_uuid.try_into()?,
+			user_uuid: if user_uuid.is_null() {
+				None
+			} else {
+				Some(user_uuid.try_into()?)
+			},
+			user_name,
+			message,
+			created_at: created_at.try_into()?,
+		})
+	}
 }
--- communityvi-server/src/database/libsql/chat.rs
+++ communityvi-server/src/database/libsql/chat.rs
@@ -2,22 +2,47 @@ use crate::chat::model::ChatMessage;
 use crate::chat::repository::ChatRepository;
 use crate::database::Connection;
 use crate::database::error::DatabaseError;
-use crate::database::libsql::LibSqlRepository;
+use crate::database::libsql::{LibSqlRepository, libsql_connection};
+use crate::types::date_time::DateTime;
 use crate::types::uuid::Uuid;
+use anyhow::anyhow;
 use async_trait::async_trait;
-use chrono::{DateTime, Utc};
 
 #[async_trait]
 impl ChatRepository for LibSqlRepository {
 	async fn create(
 		&self,
-		_connection: &mut dyn Connection,
-		_room_uuid: Uuid,
-		_user_uuid: Uuid,
-		_user_name: String,
-		_message: String,
-		_created_at: DateTime<Utc>,
+		connection: &mut dyn Connection,
+		room_uuid: Uuid,
+		user_uuid: Uuid,
+		user_name: String,
+		message: String,
+		created_at: DateTime,
 	) -> Result<ChatMessage, DatabaseError> {
-		unimplemented!()
+		let connection = libsql_connection(connection)?;
+
+		let uuid = Uuid::new_v4();
+		let mut rows = connection
+			.query(
+				"INSERT INTO chat_message(
+				uuid, room_uuid, user_uuid, user_name, message, created_at
+			) VALUES (?1, ?2, ?3, ?4, ?5, ?6)
+			RETURNING
+				uuid,
+				room_uuid,
+				user_uuid,
+				user_name,
+				message,
+				created_at
+			",
+				(uuid, room_uuid, user_uuid, user_name, message, created_at),
+			)
+			.await?;
+
+		rows.next()
+			.await?
+			.ok_or_else(|| DatabaseError::NotFound(anyhow!("not found")))?
+			.try_into()
+			.map_err(DatabaseError::Decode)
 	}
 }
